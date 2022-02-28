7dd6910b7ce301d5cb023c07ec80f341b3513f07
ogty <mofuiti1803@gmail.com>
2022-03-01 06:41:41 +0900

    fix: variables name

--- src/modules/ci.rs
+++ src/modules/ci.rs
@@ -38,13 +38,6 @@ pub fn read_lines(path: &str) -> Vec<String> {
 }
 
 
-//
-//
-// TODO: Change the variable name
-//
-// ||||||||||||||||||||||||||||||
-// vvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-
 pub fn ci(path: &str, is_relative_path: bool) {
     let languages: Vec<&str> = vec![
         "python", 
@@ -84,12 +77,9 @@ pub fn ci(path: &str, is_relative_path: bool) {
 
     let language_comment_map: HashMap<_, _> = languages.iter().zip(single_line_comment_out_prefix.iter()).collect();
 
-    
     let mut article_script_path = String::from("");
     let mut article_path = String::from("");
     let mut project_path = String::from("");
-
-
     if is_relative_path {
         article_script_path = format!("{}.txt", path);
         article_path = format!("{}.md", path);
@@ -100,12 +90,12 @@ pub fn ci(path: &str, is_relative_path: bool) {
     }
     
     let re_code_snippet: Regex = Regex::new(r"```(?P<lang>\w+):(?P<path>.*)").unwrap();
-    let xxx: Regex = Regex::new(r"```(?P<lang>\w+):(?P<path>.*):(?P<message>.*)").unwrap();
     let mut article_line_count: usize = 0;
 
     let mut article_data: Vec<String> = read_lines(&article_script_path);
 
     let mut matched_indexes = Vec::new();
+    let code_block_start_indexes = matched_indexes.clone();
     for article_line in &article_data {
         if re_code_snippet.is_match(article_line) {
             matched_indexes.push(article_line_count);
@@ -113,83 +103,82 @@ pub fn ci(path: &str, is_relative_path: bool) {
         article_line_count += 1;
     }
 
-    let a = matched_indexes.clone();
     
     let mut result = Vec::new();
     let mut for_playground = Vec::new();
 
     for matched_index in matched_indexes {
-        let tmp = article_data[matched_index].to_string();
+        let code_block_start_line = article_data[matched_index].to_string();
 
-        let splited_tmp: Vec<&str> = tmp.split(":").collect();
-        let lang = splited_tmp[0].replace("```", "");
+        let splited_code_block_start_line: Vec<&str> = code_block_start_line.split(":").collect();
+        let lang = splited_code_block_start_line[0].replace("```", "");
         let comment_out_prefix = language_comment_map.get(&lang.as_str()).unwrap();
 
-        // let source_code_path: String = format!("{}/{}", project_path, splited_tmp[1]);
         let source_code_path: String = if is_relative_path {
-            splited_tmp[1].to_string()
+            splited_code_block_start_line[1].to_string()
         } else {
-            format!("{}/{}", project_path, splited_tmp[1])
+            format!("{}/{}", project_path, splited_code_block_start_line[1])
         };
 
-        if splited_tmp.len() == 3 {
+        if splited_code_block_start_line.len() == 3 {
             let source_code_data: Vec<String> = read_lines(&source_code_path);
             let code_block_number = &article_data[matched_index + 1];
             let mut count: usize = 0;
-            let mut tmp = Vec::new();
+            let mut comment_out_start_end = Vec::new();
             for source_code_line in &source_code_data {
                 if source_code_line == &format!("{} {}", comment_out_prefix, code_block_number) {
-                    tmp.push(count);
+                    comment_out_start_end.push(count);
                 } else if source_code_line == &format!("{} -{}", comment_out_prefix, code_block_number) {
-                    tmp.push(count);
+                    comment_out_start_end.push(count);
                 }
                 count += 1;
             }
-            let source = source_code_data[(tmp[0] + 1)..tmp[1]].join("\n");
+            let source = source_code_data[(comment_out_start_end[0] + 1)..comment_out_start_end[1]].join("\n");
             let cloned_source = source.clone();
             
             if lang == "rust" {
-                for_playground.push(format!("[{}](https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&code={})\n", splited_tmp[2], encode(&cloned_source)));
+                for_playground.push(format!("[{}](https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&code={})\n", splited_code_block_start_line[2], encode(&cloned_source)));
             } 
 
             result.push(source);
         } else {
-            // let source_code_path: String = format!("{}/{}", project_path, splited_tmp[1]);
             let source_code_data: Vec<String> = read_lines(&source_code_path);
             let code_block_number = &article_data[matched_index + 1];
             let mut count: usize = 0;
-            let mut tmp = Vec::new();
+            let mut comment_out_start_end = Vec::new();
             for source_code_line in &source_code_data {
                 if source_code_line == &format!("{} {}", comment_out_prefix, code_block_number) {
-                    tmp.push(count);
+                    comment_out_start_end.push(count);
                 } else if source_code_line == &format!("{} -{}", comment_out_prefix, code_block_number) {
-                    tmp.push(count);
+                    comment_out_start_end.push(count);
                 }
                 count += 1;
             }
-            let source = source_code_data[(tmp[0] + 1)..tmp[1]].join("\n");
+            let source = source_code_data[(comment_out_start_end[0] + 1)..comment_out_start_end[1]].join("\n");
             result.push(source);
         }
     }
 
-    for (j, k) in izip!(a, result) {
-        article_data.remove(j + 1);
-        article_data.insert(j + 1, k);
+    for (code_block_start_index, source_code) in izip!(code_block_start_indexes, result) {
+        article_data.remove(code_block_start_index + 1);
+        article_data.insert(code_block_start_index + 1, source_code);
     }
 
-    let mut zzz = 0;
-    let mut yyy = Vec::new();
+    let mut index = 0;
+    let mut indexes_to_add_url = Vec::new();
+    let re_for_playground: Regex = Regex::new(r"```(?P<lang>\w+):(?P<path>.*):(?P<message>.*)").unwrap();
     for article_line in &article_data {
-        if xxx.is_match(article_line) {
-            yyy.push(zzz);
+        if re_for_playground.is_match(article_line) {
+            indexes_to_add_url.push(index);
         }
-        zzz += 1;
+        index += 1;
     }
 
-    let mut t = 0;
-    for (j, k) in izip!(yyy, for_playground) {
-        article_data.insert(j + t , k);
-        t += 1;
+    let mut index_adjustment = 0;
+    for (index_to_add_url, playground_url) in izip!(indexes_to_add_url, for_playground) {
+        article_data.insert(index_to_add_url + index_adjustment , playground_url);
+        index_adjustment += 1;
     }
+
     write(&article_path, article_data.join("\n"));
 }
