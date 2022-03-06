4ec6e583400f1930e1a00d3d1432794e98753fb3
Yoshinobu Ogata <92501820+ogty@users.noreply.github.com>
2022-03-06 11:04:27 +0900

    Update eq.sh

--- eq.sh
+++ eq.sh
@@ -1,10 +1,11 @@
+#!/bin/bash -
+
 START=$1
 END=$2
 FILENAME=$3
 
 if [[ -e $FILENAME ]]
 then 
-    echo $FILENAME exists
     awk "/$START/,/$END/"'{print $0}' $FILENAME
 else
     echo "$FILENAME is not defined"
